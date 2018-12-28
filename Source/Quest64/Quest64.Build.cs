9a96dbed6fc49c7f6a35d502526672f67a6dfb35
Lordmatics <niallmaple@yahoo.co.uk>
2018-12-28 23:43:47 +0000

    Added Modules
    Navigation System
    AIModule
    Slate
    SlateCore
    Remove VR

--- Source/Quest64/Quest64.Build.cs
+++ Source/Quest64/Quest64.Build.cs
@@ -8,6 +8,23 @@ public class Quest64 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
-	}
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "NavigationSystem", "AIModule" });
+
+        // Useful links
+        // https://docs.unrealengine.com/en-us/Programming/Slate/InGameUI
+        // https://docs.unrealengine.com/en-us/Programming/Slate/Overview
+        // https://docs.unrealengine.com/en-us/Programming/Slate
+        // https://wiki.unrealengine.com/Slate,_Hello
+        PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        // if ((Target.Platform == UnrealTargetPlatform.Win32) || (Target.Platform == UnrealTargetPlatform.Win64))
+        // {
+        //		if (UEBuildConfiguration.bCompileSteamOSS == true)
+        //		{
+        //			DynamicallyLoadedModuleNames.Add("OnlineSubsystemSteam");
+        //		}
+        // }
+    }
 }
